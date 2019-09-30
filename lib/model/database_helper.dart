d004b36055d70a3d9b92c45902fe702349258b82
Rundong Du <rundongdu@google.com>
2019-09-30 15:46:51 -0700

    Added more comments for database description and creation. (WIP #1)

--- lib/model/database_helper.dart
+++ lib/model/database_helper.dart
@@ -1,18 +1,29 @@
 import 'package:meta/meta.dart';
 import 'package:taqo_survey/util/table_util.dart';
 
+/// Description of databases
+/// We describe a database using meta information such as version and the
+/// descriptions of each DB table. Each DB table is described by a traditional
+/// table of type [Table]. The head of [Table] object decides what information
+/// of the DB table is provided.
 class DatabaseDescription {
+  /// Meta information
   final Map<String, dynamic> meta;
+
+  /// The default [Table] head, used by [.addTable()] if a custom head is not specified
   final List<String> defaultHead;
+
+  /// A map from DB table name to [Table] object as the description of that DB table
   Map<String, Table> tableDescriptions = {};
 
   DatabaseDescription(
       {this.defaultHead = const ['columnName', 'columnType'], this.meta});
 
-  void addTable(
-      {@required String name,
-      List<String> withCustomHead,
-      List<dynamic> description}) {
+  void addTable({
+    @required String name, // DB table name
+    List<String> withCustomHead, // Custom head of [Table] object
+    List<dynamic> description, // The [body] of the description table
+  }) {
     withCustomHead ??= defaultHead;
     tableDescriptions[name] = Table(head: withCustomHead, body: description);
   }
--- lib/util/table_util.dart
+++ lib/util/table_util.dart
@@ -2,9 +2,16 @@ import 'package:meta/meta.dart';
 
 /// A table in traditional meaning, with rows and columns.
 class Table {
+  /// Table head (column names)
   List<String> head;
+
+  /// The content of the table stored in a row major order, should be of size rowCount x columnCount.
   List<dynamic> body;
+
+  /// The number of columns in the table
   int columnCount;
+
+  /// A map from column name in the head to the column index (starting from 0)
   Map<String, int> headToIndexMap = {};
 
   Table({@required this.head, this.body}) {
@@ -14,6 +21,8 @@ class Table {
     }
   }
 
+  /// Get a row iterator, where each row is represented by a map with table
+  /// head/column name as key and the actual table entry as value.
   Iterable<Map<String, dynamic>> get rowIterator sync* {
     var rowCount = body.length ~/
         columnCount; // Incomplete last row, if existing, will be ignored
