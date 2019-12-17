bac2e73cc51cb14c4d78dafb72977aea04a6a560
Mike Maksymowych <maksymowych@google.com>
2019-12-16 16:57:50 -0800

    cleans up post_join_instructions_page.dart
    run dartfmt, imports, etc.

--- taqo_client/lib/pages/post_join_instructions_page.dart
+++ taqo_client/lib/pages/post_join_instructions_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:taqo_client/model/experiment.dart';
 import 'package:taqo_client/pages/running_experiments_page.dart';
-import 'package:flutter_html/flutter_html.dart';
 
 class PostJoinInstructionsPage extends StatefulWidget {
   static const routeName = '/post_join_instructions';
@@ -12,45 +12,43 @@ class PostJoinInstructionsPage extends StatefulWidget {
   _PostJoinInstructionsPageState createState() => _PostJoinInstructionsPageState();
 }
 
-
 class _PostJoinInstructionsPageState extends State<PostJoinInstructionsPage> {
-
   @override
   Widget build(BuildContext context) {
     Experiment experiment = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(experiment.title + " Instructions"),
-        backgroundColor: Colors.indigo,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(8.0),
-        //margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-        child: Column(children: <Widget>[Expanded(child: buildinstructionsColumn(experiment),)]),
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.done),
-          onPressed: () {
-        Navigator.pushReplacementNamed(context, RunningExperimentsPage.routeName);
-      })
+        appBar: AppBar(
+          title: Text("${experiment.title} Instructions"),
+          backgroundColor: Colors.indigo,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(8.0),
+          child: Column(children: <Widget>[
+            Expanded(
+              child: _buildInstructionsColumn(experiment),
+            ),
+          ]),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.done),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, RunningExperimentsPage.routeName);
+            },
+        )
     );
   }
 
+  Widget _buildInstructionsColumn(Experiment experiment) {
+    final data = experiment.postInstallInstructions != null
+        ? experiment.postInstallInstructions
+        : "No further instructions provided";
 
-  Widget buildinstructionsColumn(Experiment experiment) {
-    final data = experiment.postInstallInstructions != null ?
-        experiment.postInstallInstructions :
-        "No further instructions provided";
-
-    return SingleChildScrollView(child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text("Post Join Instructions", style: TextStyle(fontWeight: FontWeight.bold),),
-          Divider(height: 16.0, color: Colors.black,),
-          Html(data: data,),
-        ]
-      )
-    );
+    return SingleChildScrollView(child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Post Join Instructions", style: TextStyle(fontWeight: FontWeight.bold)),
+        Divider(height: 16.0, color: Colors.black),
+        Html(data: data,),
+      ]
+    ));
   }
-
 }
