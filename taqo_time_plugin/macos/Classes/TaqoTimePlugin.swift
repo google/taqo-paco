aa353b7a8ce68286b46ef30b4d9884bbc3e71a9c
Michael Maksymowych <maksymowych@maksymowych-macbookpro.roam.corp.google.com>
2020-02-07 11:20:44 -0800

    Add non-functional macOS support
    Resolves buil/run errors, but Flutter on macOS currently does not
    support callbacks on a background Isolate

--- taqo_time_plugin/macos/Classes/TaqoTimePlugin.swift
+++ taqo_time_plugin/macos/Classes/TaqoTimePlugin.swift
@@ -1,18 +1,42 @@
 import Cocoa
 import FlutterMacOS
+import os
+
+private let CALLBACK_HANDLE = "callback"
+private let BG_CALLBACK_HANDLE = "background_callback"
+
+private func _log(_ args: CVarArg...) {
+  if #available(macOS 10.12, *) {
+    os_log("TaqoTimePlugin: %s", args)
+  }
+}
 
 public class TaqoTimePlugin: NSObject, FlutterPlugin {
+  private static let channelName = "taqo_time_plugin"
+  private static let initialize = "initialize"
+  private static let cancel = "cancel"
+
+  private static let bgCallbackMethod = "backgroundIsolateCallback"
+
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "taqo_time_plugin", binaryMessenger: registrar.messenger)
+    let channel = FlutterMethodChannel(name: channelName, binaryMessenger: registrar.messenger)
     let instance = TaqoTimePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  @objc private func timeChanged(notification: NSNotification) {
+    _log("timeChanged")
+  }
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-      case initialize:
+    case TaqoTimePlugin.initialize:
+        NotificationCenter.default.addObserver(self, selector: #selector(self.timeChanged), name: NSNotification.Name.NSSystemClockDidChange, object: nil)
+
         result(true)
-      case cancel:
+      case TaqoTimePlugin.cancel:
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.NSSystemClockDidChange, object: nil)
+
         result(true)
       default:
         result(FlutterMethodNotImplemented)
