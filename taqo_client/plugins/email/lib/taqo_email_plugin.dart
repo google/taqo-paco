1eec34e66d4cf22248875d6d961458797107bc5f
Mike Maksymowych <maksymowych@google.com>
2020-05-05 19:30:14 -0700

    Enable web-based email link on Linux
    - Show a dialog prompting the user to open their email
    - Also provides manual instructions for contacting the researcher,
       in case we fail to open the email client e.g.

--- taqo_client/plugins/email/lib/taqo_email_plugin.dart
+++ taqo_client/plugins/email/lib/taqo_email_plugin.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 const _channelName = "taqo_email_plugin";
 const _sendEmailMethod = "send_email";
@@ -9,10 +11,27 @@ const _subjectArg = "subject";
 
 const MethodChannel _channel = const MethodChannel(_channelName);
 
-Future<void> sendEmail(String to, String subject) => _channel
-    .invokeMethod(_sendEmailMethod, {
-      _toArg: to,
-      _subjectArg: subject,
-    })
-    .then((value) => print("Success sending email: $value"))
-    .catchError((e, st) => print("Failed sending email: $e"));
+const gmailTemplate = ["https://mail.google.com/mail/?view=cm&fs=1&to=", "&su=", ];
+
+Future<void> sendEmail(String to, String subject) async {
+  if (Platform.isLinux) {
+    final subjEncode = Uri.encodeQueryComponent(subject);
+    final url = gmailTemplate[0] + to + gmailTemplate[1] + subjEncode;
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  } else {
+    try {
+      final res = await _channel.invokeMethod(
+          _sendEmailMethod,
+          {
+            _toArg: to,
+            _subjectArg: subject,
+          }
+      );
+      print("Success sending email: $res");
+    } on Exception catch (e) {
+      print("Failed sending email: $e");
+    }
+  }
+}
