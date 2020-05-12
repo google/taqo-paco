e3f0bf407718ff271aa25b91bd4c7bfafbdeb015
Mike Maksymowych <maksymowych@google.com>
2020-05-12 14:24:30 -0700

    Update App Loggers
    - Use shared Event model
    - Insert into DB instead of RPC (daemon and pal are
        the same process now)

---
+++ pal_event_server/lib/src/linux_daemon/loggers/pal_event_helper.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+
+import 'package:taqo_common/model/event.dart';
+import 'package:taqo_common/model/experiment.dart';
+import 'package:taqo_common/util/zoned_date_time.dart';
+
+import '../../sqlite_database/sqlite_database.dart';
+import 'app_logger.dart' show appNameField, windowNameField;
+
+const _participantId = 'participantId';
+
+const _responseName = 'name';
+const _responseAnswer = 'answer';
+
+Event _createPacoEvent(Experiment experiment, String groupName) {
+  var group;
+  try {
+    group = experiment.groups.firstWhere((g) => g.name == groupName);
+  } catch (e) {
+    print('Failed to get experiment group $groupName for $experiment');
+  }
+
+  final event = Event.of(experiment, group);
+  event.responseTime = ZonedDateTime.now();
+
+  event.responses = <String, dynamic>{
+      _responseName: _participantId,
+      _responseAnswer: 'TODO:participantId',
+  };
+
+  return event;
+}
+
+const _appsUsedKey = 'apps_used';
+const _appContentKey = 'app_content';
+const _appsUsedRawKey = 'apps_used_raw';
+
+Future<Event> createAppUsagePacoEvent(Experiment experiment, String groupName,
+    Map<String, dynamic> response) async {
+  final event = await _createPacoEvent(experiment, groupName);
+  final responses = <String, dynamic>{
+      _appsUsedKey: response[appNameField],
+      _appContentKey: response[windowNameField],
+      _appsUsedRawKey: '${response[appNameField]}:${response[windowNameField]}',
+  };
+  event.responses.addAll(responses);
+  return event;
+}
+
+const _uidKey = 'uid';
+const _pidKey = 'pid';
+const _cmdRawKey = 'cmd_raw';
+const _cmdRetKey = 'cmd_ret';
+
+Future<Event> createCmdUsagePacoEvent(Experiment experiment, String groupName,
+    Map<String, dynamic> response) async {
+  final event = await _createPacoEvent(experiment, groupName);
+  final responses = <String, String>{
+      _uidKey: response[_uidKey],
+      _pidKey: '${response[_pidKey]}',
+      _cmdRetKey: '${response[_cmdRetKey]}',
+      _cmdRawKey: response[_cmdRawKey].trim(),
+  };
+  event.responses.addAll(responses);
+  return event;
+}
+
+void storePacoEvent(List<Event> events) async {
+  final database = await SqliteDatabase.get();
+  for (var e in events) {
+    print('storeEvent: $e');
+    await database.insertEvent(e);
+  }
+}
