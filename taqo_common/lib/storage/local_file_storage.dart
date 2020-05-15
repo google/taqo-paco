71feeda7756e24aebaf4769393730c71af0e44d2
Rundong Du <rundongdu@google.com>
2020-05-15 14:22:44 -0700

    Added LocalFileStorageFactory.

--- taqo_common/lib/storage/local_file_storage.dart
+++ taqo_common/lib/storage/local_file_storage.dart
@@ -9,3 +9,21 @@ abstract class ILocalFileStorage {
 
   Future clear();
 }
+
+typedef LocalFileStorageFactoryFunction = ILocalFileStorage Function(String);
+
+class LocalFileStorageFactory {
+  static LocalFileStorageFactoryFunction _factory;
+  static Directory _localStorageDirectory;
+
+  static Directory get localStorageDirectory => _localStorageDirectory;
+
+  static initialize(LocalFileStorageFactoryFunction factory, Directory localStorageDirectory) {
+    _factory = factory;
+    _localStorageDirectory = localStorageDirectory;
+  }
+
+  static makeLocalFileStorage(String filename) {
+    return _factory(filename);
+  }
+}
