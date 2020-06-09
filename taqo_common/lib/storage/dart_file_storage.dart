9b3c3c8b3f5c9b95bccf8ffc4a9495d0d4fe49c2
Mike Maksymowych <maksymowych@google.com>
2020-06-09 09:34:02 -0700

    Update macOS storage dir

--- taqo_common/lib/storage/dart_file_storage.dart
+++ taqo_common/lib/storage/dart_file_storage.dart
@@ -7,12 +7,14 @@ import '../storage/local_file_storage.dart';
 class DartFileStorage implements ILocalFileStorage {
   final _localFileName;
 
+  // These should match whatever Flutter path_provider returns
+  // Is there a better way to keep them in sync?
   static Directory getLocalStorageDir() {
     if (Platform.isLinux) {
       return Directory('${Platform.environment['HOME']}/.taqo');
     } else if (Platform.isMacOS) {
       return Directory(
-          '${Platform.environment['HOME']}/Library/Containers/com.taqo.survey.taqoClient/Data/Documents');
+          '${Platform.environment['HOME']}/Library/Containers/com.taqo.survey.taqoClient/Data/Library/Application Support/com.taqo.survey.taqoClient');
   }
 
     throw UnsupportedError('Only supported on desktop platforms');
