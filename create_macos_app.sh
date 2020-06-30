aa645d4bac340358528bdda374427c7832a3c2b9
Mike Maksymowych <maksymowych@google.com>
2020-06-30 16:10:13 -0700

    Auto-launch taqo_daemon on macOS
    WIP
    
    Things missing:
    - Need to somehow make sure taqo_daemon launches before taqo_client checks
        if it is running
    - Don't launch taqo_daemon if it is already running

--- create_macos_app.sh
+++ create_macos_app.sh
@@ -23,7 +23,7 @@ ${DART_SDK}/bin/dart2native -p pal_event_server/.packages \
   pal_event_server/lib/main.dart
 
 # cp daemon to Flutter asset
-cp ${RELEASE}/taqo_daemon taqo_client/assets/taqo_daemon
+cp ${RELEASE}/taqo_daemon taqo_client/macos/TaqoLauncher/taqo_daemon
 
 # zip/cp intellij to Flutter asset
 if [ ! -d pal_intellij_plugin/out ]; then
@@ -44,7 +44,7 @@ popd || exit
 
 # Build flutter app
 pushd taqo_client || exit
-${FLUTTER_SDK}/bin/flutter clean && ${FLUTTER_SDK}/bin/flutter build macos
+${FLUTTER_SDK}/bin/flutter clean && ${FLUTTER_SDK}/bin/flutter -v build macos
 popd || exit
 
 # rm assets after build
--- taqo_client/macos/Runner/AppDelegate.swift
+++ taqo_client/macos/Runner/AppDelegate.swift
@@ -1,8 +1,33 @@
 import Cocoa
 import FlutterMacOS
+import ServiceManagement
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+  override func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Seting LoginItem -> True
+    let launcherAppId = "com.taqo.survey.TaqoLauncher"
+    SMLoginItemSetEnabled(launcherAppId as CFString, true)
+
+    let taqoUrl = URL(string: "taqo://")
+    guard let url = taqoUrl else {
+      NSLog("Failed to get URL for Taqo")
+      return
+    }
+
+    if #available(OSX 10.15, *) {
+      let conf = NSWorkspace.OpenConfiguration()
+      NSWorkspace.shared.openApplication(at: url, configuration: conf, completionHandler: { (app: NSRunningApplication?, err: Error?) -> Void in
+        NSLog("Something happened")
+        if let _ = app {
+          NSLog("OK")
+        } else if let _ = err {
+          NSLog("ERR")
+        }
+      })
+    }
+  }
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
