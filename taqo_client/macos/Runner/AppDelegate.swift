427aa36e6770f61c14c3d5c1eecb81095abe16c4
Mike Maksymowych <maksymowych@google.com>
2020-06-30 19:19:46 -0700

    Use UserDefaults to track first launch
    TODO: Alternatively, can we detect if taqo_daemon (not TaqoLauncher)
    is running?

--- taqo_client/macos/Runner/AppDelegate.swift
+++ taqo_client/macos/Runner/AppDelegate.swift
@@ -2,6 +2,8 @@ import Cocoa
 import FlutterMacOS
 import ServiceManagement
 
+private let hasBeenLaunchedBefore = "isFirstLaunch"
+
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationDidFinishLaunching(_ aNotification: Notification) {
@@ -9,22 +11,29 @@ class AppDelegate: FlutterAppDelegate {
     let launcherAppId = "com.taqo.survey.TaqoLauncher"
     SMLoginItemSetEnabled(launcherAppId as CFString, true)
 
-    let taqoUrl = URL(string: "taqo://")
-    guard let url = taqoUrl else {
-      NSLog("Failed to get URL for Taqo")
-      return
-    }
+    let prefs = UserDefaults.standard
+    let wasPrevLaunched = prefs.bool(forKey: hasBeenLaunchedBefore)
+//    let REMOVE_AFTER_DEBUG = true
+    if /*REMOVE_AFTER_DEBUG ||*/ !wasPrevLaunched {
+      prefs.set(true, forKey: hasBeenLaunchedBefore)
 
-    if #available(OSX 10.15, *) {
-      let conf = NSWorkspace.OpenConfiguration()
-      NSWorkspace.shared.openApplication(at: url, configuration: conf, completionHandler: { (app: NSRunningApplication?, err: Error?) -> Void in
-        NSLog("Something happened")
-        if let _ = app {
-          NSLog("OK")
-        } else if let _ = err {
-          NSLog("ERR")
-        }
-      })
+      let taqoUrl = URL(string: "taqo://")
+      guard let url = taqoUrl else {
+        NSLog("Failed to get URL for Taqo")
+        return
+      }
+
+      if #available(OSX 10.15, *) {
+        let conf = NSWorkspace.OpenConfiguration()
+        NSWorkspace.shared.openApplication(at: url, configuration: conf, completionHandler: { (app: NSRunningApplication?, err: Error?) -> Void in
+  //        NSLog("Something happened")
+          if let _ = app {
+  //          NSLog("OK")
+          } else if let _ = err {
+  //          NSLog("ERR")
+          }
+        })
+      }
     }
   }
 
