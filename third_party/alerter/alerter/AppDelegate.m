b8a063fe42388c84d4c0207aee0c6e83e71dd239
Rundong Du <rundongdu@google.com>
2021-03-02 09:03:42 -0800

    Move macos/alerter to third_party

--- third_party/alerter/alerter/AppDelegate.m
+++ third_party/alerter/alerter/AppDelegate.m
@@ -1,17 +1,3 @@
-// Copyright 2021 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #import "AppDelegate.h"
 #import <objc/runtime.h>
 
@@ -146,6 +132,7 @@ isMavericks()
            "       -contentImage URL  The URL of a image to display attached to the notification (Mavericks+ only)\n" \
            "       -json       Write only event or value to stdout \n" \
            "       -timeout NUMBER    Close the notification after NUMBER seconds.\n" \
+           "       -ignoreDnD         Send notification even if Do Not Disturb is enabled.\n" \
            "\n" \
            "When the user activates or close a notification, the results are logged to stdout as a json struct.\n" \
            "\n" \
@@ -236,7 +223,9 @@ isMavericks()
         
         options[@"uuid"] = [NSString stringWithFormat:@"%ld", self.hash] ;
         
-
+        if([[[NSProcessInfo processInfo] arguments] containsObject:@"-ignoreDnD"] == true) {
+          options[@"ignoreDnD"] = @YES;
+        }
         
         [self deliverNotificationWithTitle:defaults[@"title"] ?: @"Terminal"
                                   subtitle:subtitle
@@ -318,6 +307,10 @@ isMavericks()
     
     if (sound != nil) {
         userNotification.soundName = [sound isEqualToString: @"default"] ? NSUserNotificationDefaultSoundName : sound ;
+    }
+    
+    if(options[@"ignoreDnD"]){
+      [userNotification setValue:@YES forKey:@"_ignoresDoNotDisturb"];
     }
     
     NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
