1be89f4a06d9115c4a712fac82afa362c450a45d
dileeps861 <dileepshah@google.com>
2022-02-18 13:21:54 -0500

    CI script for testing

---
+++ taqo_client/build_test.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Setting colors 
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+none=$(tput sgr0)
+
+# Check if flutter is installed, if not, install the flutter
+if ! type flutter >/dev/null; then
+  cd ../..
+#   pwd
+  git clone https://github.com/flutter/flutter.git -b stable
+  export PATH="$PATH:/tmpfs/src/github/flutter/bin"
+#   ls
+#   echo "$PATH"
+  flutter doctor
+  cd taqo-paco/taqo_client || none
+fi
+
+# Run test cases
+run_tests() {
+  if [[ -f "pubspec.yaml" ]]; then
+    rm -f coverage/lcov.info
+    rm -f coverage/lcov-final.info
+    flutter test
+    if (( $? != 0 )); then
+         printf "\n${red}Error: Some test cases failed${none}\n"
+        exit 1
+    else
+        printf "\n${green} All test cases passed!${none}\n"
+    fi
+  else
+    printf "\n${red}Error: Not flutter project${none}\n"
+    exit 1
+  fi
+}
+
+run_tests
+
+
