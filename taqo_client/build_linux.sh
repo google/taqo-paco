801a855ef88074563c81ba979af51c9cc160ffb6
dileeps861 <dileepshah@google.com>
2022-02-24 10:56:19 -0500

    Added build script for gcp linux

--- taqo_client/build_linux.sh
+++ taqo_client/build_linux.sh
@@ -15,17 +15,17 @@ flutter clean
 
 # Install debhelper if not already installed
 if ! type debhelper >/dev/null; then
-    sudo apt-get install -y debhelper
+    apt-get install -y debhelper
 fi
 
 # Install Jq if not already installed
 if ! type jq >/dev/null; then
-    sudo apt-get install -y jq
+    apt-get install -y jq
 fi
 
 # Install chrpath if not already installed
 if ! type chrpath >/dev/null; then
-    sudo apt-get install -y chrpath
+    apt-get install -y chrpath
 fi
 
 # Go to root directory.
