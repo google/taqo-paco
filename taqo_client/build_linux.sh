e15d5eaa577a84a5e88e910e66915e6925c079c4
dileeps861 <dileepshah@google.com>
2022-03-07 11:30:07 -0500

    Modified build script for flutter

--- taqo_client/build_linux.sh
+++ taqo_client/build_linux.sh
@@ -35,7 +35,8 @@ which flutter
 
 # Install debhelper if not already installed
 sudo apt-get install -y debhelper
-
+sudo apt update
+sudo apt install -y dh-autoreconf=12~ubuntu16.04.1 debhelper=10.2.2ubuntu1~ubuntu16.04.1
 # Install Jq if not already installed
 if ! type jq >/dev/null; then
     sudo apt-get install -y jq
