4221938e85593cb59d728096d50868280693039d
dileeps861 <dileepshah@google.com>
2022-03-22 11:27:19 -0400

    Updated linux build script

--- taqo_client/build_linux.sh
+++ taqo_client/build_linux.sh
@@ -43,6 +43,8 @@ which flutter
 
 
 # Install debhelper if not already installed
+sudo apt-get update
+
 sudo apt-get install -y debhelper
 sudo apt update
 sudo apt install -y dh-autoreconf=12~ubuntu16.04.1 debhelper=10.2.2ubuntu1~ubuntu16.04.1
