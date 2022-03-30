b9dccc3d27fafbe7b09843b97a400cabd5b33913
dileeps861 <dileepshah@google.com>
2022-03-29 20:48:18 -0400

    Changed github scm

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -15,5 +15,5 @@ set -e
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
-cd "${KOKORO_ARTIFACTS_DIR}/github/taqo-paco/taqo_client"
+cd "${KOKORO_ARTIFACTS_DIR}/github/taqo-paco-kokoro/taqo_client"
 ./build_test.sh
