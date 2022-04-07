3d8c0ebd256658fd66b2307098e034f92d1bbd73
dileeps861 <dileepshah@google.com>
2022-04-07 15:47:02 -0400

    Removed flutter and java dependecies, moved installation to custom vm

--- kokoro/gcp_ubuntu/build.sh
+++ kokoro/gcp_ubuntu/build.sh
@@ -16,9 +16,7 @@
 # Fail on any error.
 set -e
 
-# Export the flutter which is installed in vm.
-export PATH="/home/${USER}/flutter/bin:${PATH}"
-printf "\n Path: %s\n" "${PATH}"
+
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
@@ -30,6 +28,10 @@ source deps.cfg
 # Export the java path if not already exported
 export JAVA_HOME="/usr/lib/jvm/java-${java_version}-openjdk-amd64"
 export PATH="${JAVA_HOME}/bin:{$PATH}"
+
+# Export the flutter which is installed in vm.
+export PATH="/home/${USER}/flutter/bin:${PATH}"
+printf "\n Path: %s\n" "${PATH}"
 
 # Clean previous flutter builds
 cd taqo_client
