e52417923957984bf11aeeb731b7b404c0e629a4
Rundong Du <rundongdu@google.com>
2023-08-23 15:32:08 -0700

    taqo_log_cmd always exit with zero status

--- taqo_log_cmd/bin/taqo_log_cmd.dart
+++ taqo_log_cmd/bin/taqo_log_cmd.dart
@@ -19,12 +19,16 @@ import 'package:taqo_common/rpc/rpc_constants.dart';
 import 'package:taqo_common/util/zoned_date_time.dart';
 import 'package:taqo_event_server_protocol/taqo_event_server_protocol.dart';
 
+// This program should always exit with code 0, since it will be invoked in
+// pre_cmd and pre_exec hooks, where a non-zero exit code will basically disable
+// the user's terminal.
 Future<void> main(List<String> arguments) async {
   final tespClient = TespEventClient(localServerHost, localServerPort);
   try {
-    tespClient.connect();
-  } catch (e) {
-    exit(1);
+    await tespClient.connect();
+  } catch (e, st) {
+    displayError(e, st);
+    exit(0);
   }
   ShellCommandLog cmdLog;
   if (arguments.length == 4 && arguments[0] == 'start') {
@@ -40,13 +44,46 @@ Future<void> main(List<String> arguments) async {
         exitCode: int.parse(arguments[2]));
   } else {
     final executable = Platform.script.pathSegments.last;
-    stderr.write('''Usage:
+    stderr.write('''This message is for debug/test only. 
+If you see it as a user, please file a bug at
+https://github.com/google/taqo-paco/issues
+or contact the Taqo authors.
+
+Usage:
 $executable start <command> <shell-pid> fg|bg
 or
 $executable end <shell-pid> <exit-code>
-    ''');
-    exit(2);
+''');
+    exit(0);
   }
-  await tespClient.palLogCmd(cmdLog);
+  try {
+    await tespClient.palLogCmd(cmdLog);
+  } catch (e, st) {
+    displayError(e, st);
+  }
   exit(0);
+}
+
+const _start_server_linux = '/usr/bin/taqo_daemon restart';
+const _start_server_mac =
+    'killall taqo_daemon; open /Applications/Taqo.app/Contents/Library/LoginItems/TaqoLauncher.app';
+
+String getStartServerCmd() {
+  if (Platform.isMacOS) {
+    return _start_server_mac;
+  }
+  return _start_server_linux;
+}
+
+void displayError(e, st) {
+  stderr.write('$e\n');
+  stderr.write('$st\n');
+  stderr.write(
+      '''Taqo's shell tracer cannot connect to the Taqo server. To restart the Taqo server, please run
+  ${getStartServerCmd()}
+To manually disable Taqo's shell tracer, remove the line that sources 
+  <taqo-lib-dir>/scripts/logger.bash or <taqo-lib-dir>/scripts/logger.zsh
+in ~/.bashrc or ~/.zshrc, respectively, and restart the shell.
+Please contact the Taqo authors if you have any questions.
+''');
 }
