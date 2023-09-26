88895133f1366830ae96081aef468b098174e163
Rundong Du <rundongdu@google.com>
2023-09-25 17:17:47 -0700

    [desktop tracer] better support for window title

--- pal_event_server/lib/src/allowlist_default_rules.dart
+++ pal_event_server/lib/src/allowlist_default_rules.dart
@@ -32,7 +32,7 @@ List<AllowListRule> createRules() {
   var rules = <AllowListRule>[];
   rules.add(AllowListRule.ofAppUsed(r'Google-chrome'));
   rules.add(AllowListRule.ofAppUsed(r'Google Chrome'));
-  rules.add(AllowListRule.ofAppUsed(r'Chrome'));
+  rules.add(AllowListRule.ofAppUsed(r'Google Chat'));
   rules.add(AllowListRule.ofAppUsed(r'Taqo'));
   rules.add(AllowListRule.ofAppUsed(r'Gnome terminal'));
   rules.add(AllowListRule.ofAppUsed(r'Terminal'));
